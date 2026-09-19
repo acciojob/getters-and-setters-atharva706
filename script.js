bcb0b8e516f678fa96fc4d6c65223ab80a08d6e1
Atharva Kulkarni <atharvakulkarrni@gmail.com>
2026-09-19 15:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
-//complete this code
 class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-	constructor(name , age){
-		this.name = name;
-		this.age = age;
-	}
-	getName(){
-		return this.name
-	}
+    get name() {
+        return this._name;
+    }
 
-	setAge(age){
-		this.age = age
-	}
+    set name(name) {
+        this._name = name;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        this._age = age;
+    }
 }
 
 class Student extends Person {
-		constructor(name ,age){
-			super(name , age);
-		}
+    constructor(name, age) {
+        super(name, age);
+    }
 
-		study(){
-			console.log(`${this.name} is studying`)
-		}
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	constructor(name ,age){
-			super(name , age);
-		}
+    constructor(name, age) {
+        super(name, age);
+    }
 
-		teach(){
-			console.log(`${this.name} is teaching`)
-		}
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
 // Do not change the code below this line
